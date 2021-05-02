e144754caa1956cdf9a2439dbd36b1b634c7d6f9
Huseyin Kabasakal <huseyinkabasakal01@gmail.com>
2021-05-02 23:14:52 +0300

    The final product.

--- main.js
+++ main.js
@@ -2,15 +2,23 @@ let arr = process.argv.slice(2);
 let output = "";
 let string = ""
 for(let i=0; i<arr.length; i++){
-    num = arr[i].split("");
-    for(let j=0; j<num.length; j++){
-        last_digit = parseInt(num[j]);
-        string+=translator(last_digit);
+    //Divide numbers into digits
+    arr[i] = arr[i].split("");
+    //Iterating through the digits of number
+    for(let j=0; j<arr[i].length; j++){
+        //Convert the input to integers since the translator funtion only recognizes integers.
+        arr[i][j] = parseInt(arr[i][j]);
+        //Combine output from the translator.
+        string+=translator(arr[i][j]);
     }
+    //Combine the strings with coma.
     output = output +","+ string;
+    //Free the string for next iteration of outer loop
     string = "";
 }
+//Stdout (slice(1) was necessary to prevent starting with a coma).
 console.log(output.slice(1));
+//Translator function to translate integers to their string values.
 function translator(digit){
 switch (digit) {
     case 0:
@@ -33,6 +41,7 @@ switch (digit) {
         return "Eight";
     case 9:
         return "Nine";
+    // Error statement if user enters something other than integers.
     default:
         return "Error, your input is not recognized. You can only enter positive integers and 0 .";
     }
